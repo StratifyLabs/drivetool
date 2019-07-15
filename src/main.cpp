e7af5c96f779613275a74e47f73c591f23205171
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2019-07-15 12:06:40 -0600

    updated various actions

--- src/main.cpp
+++ src/main.cpp
@@ -6,188 +6,146 @@
 
 #include "sl_config.h"
 
+static Printer printer;
+
 static void show_usage(const Cli & cli);
-static void execute_read_all(const Cli & cli);
-static void execute_read(const Cli & cli, mcu_pin_t pin);
-static void execute_write(const Cli & cli, mcu_pin_t pin, int value);
-static void execute_mode(const Cli & cli, mcu_pin_t pin, const var::ConstString & mode);
-static void execute_pulse(const Cli & cli, mcu_pin_t pin, int value, int duration_us);
+
+bool execute_blank_check(const Drive & drive, bool is_erase = false);
 
 int main(int argc, char * argv[]){
 	String action;
-	String port_pin;
+	String address;
 	String is_help;
-	String mode;
-	String value;
-	String duration;
-	mcu_pin_t pin;
+	String size;
+	String path;
 
 	Cli cli(argc, argv);
 	cli.set_publisher(SL_CONFIG_PUBLISHER);
 	cli.handle_version();
 
+	printer.set_verbose_level(Printer::INFO);
 
-	action = cli.get_option("action", "specify the operation read|write|readall|mode|pulse");
-	port_pin = cli.get_option("pin", "specify the port/pin combination as X.Y, e.g. --pin=2.0");
+	action = cli.get_option("action", "specify the operation blankcheck|erase|eraseall|getinfo|read");
 	is_help = cli.get_option("help", "show help options");
-	mode = cli.get_option("mode", "specify mode as float|pullup|pulldown|out|opendrain");
-	value = cli.get_option("value", "specify output value as 0|1 or operations write|pulse");
-	duration = cli.get_option("duration", "specify pulse duration in microseconds, e.g. --duration=100");
-
-	pin = Pin::from_string(port_pin);
+	address = cli.get_option("address", "specify the address to read or erase");
+	size = cli.get_option("size", "specify the number of bytes to read or erase");
+	path = cli.get_option("path", "specify the path to the drive such as /dev/drive0");
+	printer.set_verbose_level( cli.get_option("verbose") );
 
 	if( is_help.is_empty() == false ){
 		cli.show_options();
 		exit(0);
 	}
 
-	if( action == "readall" ){
-		execute_read_all(cli);
-	} else if ( action == "read" ){
-		execute_read(cli, pin);
-	} else if ( action == "write" ){
-		execute_write(cli, pin, value.to_integer());
-	} else if ( action == "mode" ){
-		execute_mode(cli, pin, mode);
-	} else if ( action == "pulse" ){
-		execute_pulse(cli, pin, value.to_integer(), duration.to_integer());
-	} else {
-		show_usage(cli);
+	Drive drive;
+
+	if( path.is_empty() ){
+		printer.error("'path' must be specified");
+		cli.show_options();
+		exit(1);
 	}
 
+	drive.set_keep_open(false);
+
+	if( drive.open(path) < 0 ){
+		printer.error("Failed to open drive at '%s'", path.cstring());
+		exit(1);
+	}
+
+	printer.open_object(action);
+
+	if( action == "blankcheck" ){
+		//erase the device
+		execute_blank_check(drive, false);
+	} else if ( action == "erase" ){
+		if( drive.erase_blocks(address.to_integer(), address.to_integer()) < 0 ){
+			printer.error("failed to erase block at address 0x%lX", address);
+		} else {
+			while( drive.is_busy() ){
+				;
+			}
+			printer.info("block erased at address 0x%lX\n", address);
+		}
+
+	} else if ( action == "eraseall" ){
+		execute_blank_check(drive, true);
+	} else if ( action == "getinfo" ){
+		DriveInfo info = drive.get_info();
+
+		printer.key("path", path);
+		printer << info;
+
+	} else if ( action == "read" ){
+
+	} else {
+		printer.close_object();
+		show_usage(cli);
+		exit(1);
+	}
+
+
+	printer.close_object();
 
 	return 0;
 }
 
 
-void execute_read_all(const Cli & cli){
-	int port_num = 0;
-	int pin;
-	u32 value;
-	//show the status of all the pins
-	printf("%s:\n", cli.name().cstring());
-	printf("       28   24   20   16   12    8    4    0\n");
-	printf("     ---- ---- ---- ---- ---- ---- ---- ----\n");
-	do {
-		Pio p(port_num);
-
-		if( (p.open() < 0) || (port_num > 10) ){
-			break;
-		}
-
-		printf("P%d | ", port_num);
-		for(pin = 0; pin < 32; pin++){
-			value = p.get_value();
-			if( value & (1<<(31-pin)) ){
-				printf("1");
-			} else {
-				printf("0");
-			}
-
-			if( (pin+1) % 4 == 0 ){
-				printf(" ");
-			}
-		}
-		printf("\n");
-
-		port_num++;
-
-	} while(1);
-
-	printf("\n");
-
-}
-
 void show_usage(const Cli & cli){
-	printf("%s usage:\n", cli.name().cstring());
+	printer.open_object(String().format("%s usage:", cli.name().cstring()));
+	printf("\n");
 	cli.show_options();
 	exit(1);
 }
 
-void execute_read(const Cli & cli, mcu_pin_t pin){
+bool execute_blank_check(const Drive &drive, bool is_erase){
+	DriveInfo info = drive.get_info();
+	const u32 page_size = 1024;
+	Data buffer(page_size);
+	Data check(page_size);
 
-	if( pin.port != 255 ){
-		Pin p(pin);
-		if( p.open(Pin::RDWR) < 0 ){
-			printf("Failed to open /dev/pio%d\n", pin.port);
-		} else {
-			printf("%s:%d.%d == %d\n", cli.name().cstring(), pin.port, pin.pin, p.get_value());
-			p.close();
+	check.fill(0xff);
+
+	printer.info("Blank checking %ld blocks", (u32)info.write_block_count());
+	for(u64 i=0; i < info.size(); i+= page_size ){
+		u32 loc = drive.loc();
+
+		if( i % 1024*1024 == 0 ){
+			printer.debug("checking block at address 0x%lX", i);
 		}
-	} else {
-		show_usage(cli);
-	}
-}
 
-void execute_write(const Cli & cli, mcu_pin_t pin, int value){
+		if( drive.read(buffer) != page_size ){
+			printer.error("failed to read drive at %ld", drive.loc());
+			return false;
+		}
 
-	if( pin.port != 255 ){
-		Pin p(pin.port, pin.pin);
-		if( p.open(Pin::RDWR) < 0 ){
-			printf("Failed to open /dev/pio%d\n", pin.port);
-		} else {
-			if( value == 0 ){
-				p.clear();
+		if( buffer != check ){
+
+			if( is_erase ){
+				if( drive.erase_blocks(loc, loc) < 0 ){
+					printer.error("failed to erase block at address %ld", loc);
+					return false;
+				}
+
+				while( drive.is_busy() ){
+					chrono::wait_milliseconds(5);
+				}
+
+				if( drive.read(loc, buffer) != page_size ){
+					printer.error("failed to read drive at %ld after erase", drive.loc());
+					return false;
+				}
+
 			} else {
-				value = 1;
-				p.set();
-			}
-			printf("%s:%d.%d -> %d\n", cli.name().cstring(), pin.port, pin.pin, value);
-			p.close();
-		}
-	} else {
-		show_usage(cli);
-	}
 
-}
-
-void execute_mode(const Cli & cli, mcu_pin_t pin, const var::ConstString & mode){
-
-	if( mode.is_empty() ){
-		show_usage(cli);
-		exit(1);
-	}
-
-	if( pin.port == 255 ){
-		show_usage(cli);
-		exit(1);
-	} else {
-		Pin p(pin.port, pin.pin);
-		if( p.open(Pin::READWRITE) < 0 ){
-			printf("Failed to open /dev/pio%d", pin.port);
-		} else {
-			if( (mode == "in") || (mode == "float") || (mode == "tri") ){
-				p.set_attributes(Pin::FLAG_SET_INPUT | Pin::FLAG_IS_FLOAT);
-				printf("%s:%d.%d -> in\n", cli.name().cstring(), pin.port, pin.pin);
-			} else if ( mode == "out" ){
-				p.set_attributes(Pin::FLAG_SET_OUTPUT);
-				printf("%s:%d.%d -> out\n", cli.name().cstring(), pin.port, pin.pin);
-			} else if ( (mode == "up") || (mode == "pullup") ){
-				p.set_attributes(Pin::FLAG_SET_INPUT | Pin::FLAG_IS_PULLUP);
-				printf("%s:%d.%d -> pullup\n", cli.name().cstring(), pin.port, pin.pin);
-			} else if ( (mode == "down") || (mode == "pulldown") ){
-				p.set_attributes(Pin::FLAG_SET_INPUT | Pin::FLAG_IS_PULLDOWN);
-				printf("%s:%d.%d -> pulldown\n", cli.name().cstring(), pin.port, pin.pin);
+				printer.error("drive is not blank at %ld", drive.loc());
+				return false;
 			}
 		}
+
 	}
+
+	return true;
 }
 
-void execute_pulse(const Cli & cli, mcu_pin_t pin, int value, int duration_us){
-	if( pin.port == 0xff ){
-		show_usage(cli);
-	} else {
-		Pin p(pin.port, pin.pin);
-		if( p.open(Pin::READWRITE) < 0 ){
-			printf("Failed to open /dev/pio%d", pin.port);
-		} else {
-			//go high then low
-			printf("%s:%d.%d -> %d (%dusec) -> %d\n", cli.name().cstring(), pin.port, pin.pin, value, duration_us, !value);
-			p = (value != 0);
-			Timer::wait_microseconds(duration_us);
-			p = (value == 0);
-		}
-	}
-}
 
 
