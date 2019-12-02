7648e235cb6302b0b8b77e1be3444f8f399739fa
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2019-12-02 14:36:52 -0700

    API updates

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <sapi/sys.hpp>
 #include <sapi/hal.hpp>
 #include <sapi/var.hpp>
+#include <sapi/chrono.hpp>
 
 #include "sl_config.h"
 
@@ -26,12 +27,36 @@ int main(int argc, char * argv[]){
 
 	printer.set_verbose_level(Printer::INFO);
 
-	action = cli.get_option("action", "specify the operation blankcheck|erase|eraseall|erasedevice|getinfo|read|reset");
-	is_help = cli.get_option("help", "show help options");
-	address = cli.get_option("address", "specify the address to read or erase");
-	size = cli.get_option("size", "specify the number of bytes to read or erase");
-	path = cli.get_option("path", "specify the path to the drive such as /dev/drive0");
-	printer.set_verbose_level( cli.get_option("verbose") );
+	action = cli.get_option(
+				("action"),
+				Cli::Description("specify the operation blankcheck|erase|eraseall|erasedevice|getinfo|read|reset")
+				);
+
+	is_help = cli.get_option(
+				("help"),
+				Cli::Description("show help options")
+				);
+
+	address = cli.get_option(
+				("address"),
+				Cli::Description("specify the address to read or erase")
+				);
+
+	size = cli.get_option(
+				("size"),
+				Cli::Description("specify the number of bytes to read or erase")
+				);
+
+	path = cli.get_option(
+				("path"),
+				Cli::Description("specify the path to the drive such as /dev/drive0")
+				);
+
+	printer.set_verbose_level(
+				cli.get_option(
+					("verbose")
+					)
+				);
 
 	if( is_help.is_empty() == false ){
 		cli.show_options();
@@ -48,7 +73,7 @@ int main(int argc, char * argv[]){
 
 	drive.set_keep_open(false);
 
-	if( drive.open(path) < 0 ){
+	if( drive.initialize(path) < 0 ){
 		printer.error("Failed to open drive at '%s'", path.cstring());
 		exit(1);
 	}
@@ -70,7 +95,7 @@ int main(int argc, char * argv[]){
 				printer.error("failed to erase block at address 0x%lX", address);
 			} else {
 				while( drive.is_busy() ){
-					chrono::wait_milliseconds(1);
+					chrono::wait(Milliseconds(1));
 				}
 				printer.info("block erased at address 0x%lX\n", address);
 			}
@@ -93,7 +118,7 @@ int main(int argc, char * argv[]){
 
 			printer.info("waiting for erase to complete");
 			while( drive.is_busy() ){
-				chrono::wait_milliseconds(1);
+				chrono::wait(Milliseconds(1));
 			}
 
 			printer.info("device erase complete");
@@ -140,15 +165,15 @@ void show_usage(const Cli & cli){
 
 bool execute_read(const Drive & drive, u32 address, u32 size){
 	const u32 page_size = 1024;
-	Data buffer(page_size);
+	Data buffer = Data(page_size);
 	u32 bytes_read = 0;
-	drive.seek(address);
+	drive.seek(Device::Location(address));
 	printer.set_flags(Printer::PRINT_BLOB | Printer::PRINT_HEX);
 	do {
 		if( size - bytes_read < page_size ){
-			buffer.set_size(size - bytes_read);
+			buffer.allocate(size - bytes_read);
 		} else {
-			buffer.set_size(page_size);
+			buffer.allocate(page_size);
 		}
 		drive.read(buffer);
 		if( drive.return_value() > 0 ){
@@ -171,22 +196,41 @@ bool execute_blank_check(const Drive &drive, bool is_erase){
 	Data buffer(page_size);
 	Data check(page_size);
 
-	check.fill(0xff);
+	check.fill((u8)0xff);
+
+	drive.seek(
+				Device::Location(0)
+				);
 
 	printer.info("Blank checking %ld blocks", (u32)info.write_block_count());
 	for(u64 i=0; i < info.size(); i+= page_size ){
-		u32 loc = drive.loc();
+
+		u32 loc = drive.location();
 
 		if( (i % (1024*1024)) == 0 ){
 			printer.debug("checking block at address 0x%lX", i);
 		}
 
+		buffer.fill(0xaa);
 		if( drive.read(buffer) != page_size ){
-			printer.error("failed to read drive at %ld", drive.loc());
+			printer.error(
+						"failed to read drive at %ld",
+						drive.location()
+						);
 			return false;
 		}
 
 		if( buffer != check ){
+
+			drive.seek(
+						Device::Location(loc)
+						);
+
+			for(u32 i=0; i < buffer.count<u32>(); i++){
+				if( buffer.at_const_u32(i) != 0xffffffff ){
+					printer.debug("buffer at [%d] is 0x%08lX", i, buffer.at_const_u32(i));
+				}
+			}
 
 			if( is_erase ){
 				printer.debug("erasing block at address 0x%lX", loc);
@@ -196,21 +240,29 @@ bool execute_blank_check(const Drive &drive, bool is_erase){
 				}
 
 				while( drive.is_busy() ){
-					chrono::wait_milliseconds(5);
+					chrono::wait(Milliseconds(5));
 				}
 
-				if( drive.read(loc, buffer) != page_size ){
-					printer.error("failed to read drive at %ld after erase", drive.loc());
+				if( drive.read(
+						 Device::Location(loc),
+						 buffer
+						 ) != page_size ){
+					printer.error(
+								"failed to read drive at %ld after erase",
+								drive.location()
+								);
 					return false;
 				}
 
 			} else {
 
-				printer.error("drive is not blank at %ld", drive.loc());
+				printer.error(
+							"drive is not blank at %ld",
+							drive.location()
+							);
 				return false;
 			}
 		}
-
 	}
 
 	printer.info("drive is blank");
